18deee42f3f3af14ac0cb53a9b6f14b8b609e778
yattinda <yattinda@gmail.com>
2025-02-14 16:46:42 +0900

    Maybe work (not sure)

---
+++ handson/3_finetune.py
@@ -0,0 +1,54 @@
+from transformers import RobertaTokenizer, T5ForConditionalGeneration, Trainer, TrainingArguments, DataCollatorForSeq2Seq
+import torch
+import json
+from torch.utils.data import Dataset, DataLoader
+
+class CodeCompletionDataset(Dataset):
+    def __init__(self, file_path, tokenizer, max_length=512):
+        self.examples = []
+        with open(file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+            for pair in data:
+                input_text, target_text = pair[1], pair[0]
+                input_ids = tokenizer.encode(input_text, max_length=max_length, truncation=True, padding='max_length')
+                target_ids = tokenizer.encode(target_text, max_length=max_length, truncation=True, padding='max_length')
+                self.examples.append((input_ids, target_ids))
+
+    def __len__(self):
+        return len(self.examples)
+
+    def __getitem__(self, idx):
+        input_ids, target_ids = self.examples[idx]
+        return {
+            'input_ids': torch.tensor(input_ids),
+            'labels': torch.tensor(target_ids)
+        }
+
+tokenizer = RobertaTokenizer.from_pretrained('Salesforce/codet5-small')
+model = T5ForConditionalGeneration.from_pretrained('Salesforce/codet5-small')
+
+dataset = CodeCompletionDataset('dataset.json', tokenizer)
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+training_args = TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=3,
+    per_device_train_batch_size=8,
+    per_device_eval_batch_size=8,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=data_collator
+)
+
+trainer.train()
+
+# モデルを保存する
+model.save_pretrained('./finetuned_codet5_model')
+tokenizer.save_pretrained('./finetuned_codet5_tokenizer')
