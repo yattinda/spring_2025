29c2238255a4c95de468ab50ecc927a8387c75da
yattinda <yattinda@gmail.com>
2025-02-18 14:54:40 +0900

    Fix list error

--- handson/2_3_json_concat.py
+++ handson/2_3_json_concat.py
@@ -11,11 +11,14 @@ for filename in os.listdir(directory):
     if filename.endswith('.json'):
         with open(os.path.join(directory, filename), 'r', encoding='utf-8') as f:
             data = json.load(f)
-            merged_data.append(data)
+            # もしデータがリストであれば、リストの要素を結合
+            if isinstance(data, list):
+                merged_data.extend(data)
+            else:
+                merged_data.append(data)
 
-
+# JSON文字列として保存する際に、リストの外側の括弧を除外
 with open('../datasets/finetune_data/merged_data.json', 'w', encoding='utf-8') as f:
     json.dump(merged_data, f, ensure_ascii=False, indent=4)
 
 print('JSONファイルの結合が完了しました。')
-
